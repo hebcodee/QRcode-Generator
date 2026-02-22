3ede9d266b79f90aaf05c577b56a11a0a93acbef
hebcodee <herberth.ferreira6@gmail.com>
2026-02-22 17:43:03 -0300

    feat: implement QR code generation and URL setting

--- src/main/java/dev/heb/Main.java
+++ src/main/java/dev/heb/Main.java
@@ -1,7 +1,20 @@
 package dev.heb;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+
+        String texto = "https://www.google.com";
+
+        // Gera a imagem do QR Code
+        BufferedImage img = QrcodeGenerator.generateQrcode(texto);
+
+        // Salva no arquivo
+        ImageIO.write(img, "png", new File("qrcode.png"));
+
+        System.out.println("QR Code gerado com sucesso!");
     }
 }
